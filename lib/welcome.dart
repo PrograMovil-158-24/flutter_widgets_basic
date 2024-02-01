a10463eccc9ea620af0eb6829b808f8c0133a69b
Ivan Cruz <ivancruzhn@gmail.com>
2024-02-01 00:27:35 -0600

    fix grid error

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -4,25 +4,18 @@ import 'package:flutter_widgets_basic/widget/card_main.dart';
 class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child:
-            //LAYOUT:  COLUMN & ROW
-
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start, 
-                  children: [
-                    Row(mainAxisAlignment: MainAxisAlignment.start, 
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Image.network(
-                            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-                            width: 80,
-                            height: 80,
-                          )),
-                      const Column(children: [
+    return Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.network(
+                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                  width: 80,
+                  height: 80,
+                ),
+                const Column(children: [
                         Text('Ave exotica',
                             textAlign: TextAlign.left,
                             style:
@@ -30,35 +23,18 @@ class Welcome extends StatelessWidget {
                         Text('Conocida comunmente como Buho',
                             style: TextStyle(fontSize: 15)),
                         Text('Test!', style: TextStyle(fontSize: 20))
-                      ]),
-                    ]),
+                      ])
+              ],
+            ),
 
-              // LIST
-
-              const ListTile(
+             const ListTile(
                 leading: Icon(Icons.person),
                 title: Text('Nombre'),
                 subtitle: Text('Nombre de la persona'),
               ),
 
-            // CARD
               const CardMain(),
-            // GRIDVIEW
-
-              GridView.count(
-                // Create a grid with 2 columns. If you change the scrollDirection to
-                // horizontal, this produces 2 rows.
-                crossAxisCount: 2,
-                // Generate 100 widgets that display their index in the List.
-                children: List.generate(100, (index) {
-                  return Center(
-                    child: Text(
-                      'Item $index',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                  );
-                }),
-              )
-            ])));
+          ],
+        ));
   }
 }
